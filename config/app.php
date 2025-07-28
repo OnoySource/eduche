d08ff61d86dae9923c65c5bde120ab8d8fb5abb9
OnoySource <onoysource@gmail.com>
2025-07-28 21:15:28 +0700

    update set http https

--- config/app.php
+++ config/app.php
@@ -55,7 +55,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://eduche-production.up.railway.app'),
+
 
     'asset_url' => env('ASSET_URL'),
 
