72ee23368d3da6dc4fd3eb0f9b9fa23866de6546
OnoySource <onoysource@gmail.com>
2025-07-29 18:24:01 +0700

    add test route

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,24 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/hasil', [HomeController::class, 'hasil']);
 
 Route::post('/',[FormController::class, 'prosesForm'])->name('proses.form');
+
+Route::get('/cek-drive', function () {
+    try {
+        // Coba akses isi folder Google Drive
+        $files = Storage::disk('google')->listContents('/', false);
+
+        // Jika berhasil, tampilkan list file/folder
+        return response()->json([
+            'status' => 'berhasil',
+            'jumlah_item' => count($files),
+            'files' => $files
+        ]);
+    } catch (\Exception $e) {
+        // Jika gagal, tampilkan pesan error
+        return response()->json([
+            'status' => 'gagal',
+            'pesan' => $e->getMessage()
+        ]);
+    }
+});
+
