a53e10e901045aa16ce0de8233d453b9276858fb
OnoySource <onoysource@gmail.com>
2025-07-30 10:55:15 +0700

    update api use fonnte v2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,7 +27,15 @@ class AppServiceProvider extends ServiceProvider
         if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
+
+        // Buat symbolic link ke storage jika belum ada
+        if (!File::exists(public_path('storage'))) {
+            try {
+                Artisan::call('storage:link');
+                Log::info('Symbolic link ke storage berhasil dibuat.');
+            } catch (\Exception $e) {
+                Log::error('Gagal membuat symbolic link: ' . $e->getMessage());
+            }
+        }
     }
 }
-
-
